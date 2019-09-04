cd15eff8a771a66c4d821b5943b4d1ffb0bd0570
edean78 <edean78@gmail.com>
2019-09-04 12:57:56 -0400

    Added logic to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,16 +5,37 @@ var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var guesses = "";
+var guesses = [];
 
 // Create variables that hold references to the places in the HTML where we want to display things.
-var userGuesses = document.getElementById("guesses-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
+var guessesLeftText = document.getElementById("guessesleft-text");
 var guessesText = document.getElementById("guesses-text");
 
+// This function is run whenever the user presses a key.
 document.onkeyup = function(event){
-    var userGuess = event.key;
-}
 
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    // Determines which key was pressed.
+    var userGuess = event.key;
+
+    // Randomly chooses a choice from the options array. This is the Computer's guess.
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    if (userGuess !== computerGuess){
+        guesses.push(userGuess),
+        guessesLeft --;
+
+        if (userGuess === computerGuess){
+            wins++, guessesLeft = 9, guesses = [];
+        } else if (guessesLeft = 0) {
+            losses--, guessesLeft = 9, guesses = [];
+        }
+
+        winsText.textContent = "Wins: " + wins;
+        lossesText.textContent = "Losses: " + losses;
+        guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
+        guessesText.textContent = "Your Guesses so far: " + userGuess;
+    }
+};
+
