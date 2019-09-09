478a891ef3267a0c557b6b7cf88a1929b59d5726
edean78 <edean78@gmail.com>
2019-09-08 23:12:55 -0400

    Altered the logic if/then statements. Added span tags in the pelement to hold the wins, losses, guesses left, and guesses so far.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // Creat an array of letters the computer will choose from
-var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
 
 // Create variables to hold the value of wins, losses, guesses left, guesses so far
 var wins = 0;
@@ -13,29 +13,81 @@ var lossesText = document.getElementById("losses-text");
 var guessesLeftText = document.getElementById("guessesleft-text");
 var guessesText = document.getElementById("guesses-text");
 
+// Randomly chooses a choice from the options array. This is the Computer's guess.
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log("The computer chose " + computerGuess)
+
 // This function is run whenever the user presses a key.
-document.onkeyup = function(event){
+document.onkeyup = function (event) {
 
     // Determines which key was pressed.
     var userGuess = event.key;
+    userGuess.toLowerCase();
+    console.log("You pressed the letter " + userGuess);
 
-    // Randomly chooses a choice from the options array. This is the Computer's guess.
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    // Create the logic that handles wins and losses
 
-    if (userGuess !== computerGuess){
-        guesses.push(userGuess),
-        guessesLeft --;
-
+    if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")) {
+        
         if (userGuess === computerGuess){
-            wins++, guessesLeft = 9, guesses = [];
-        } else if (guessesLeft = 0) {
-            losses--, guessesLeft = 9, guesses = [];
+            wins++;
+            guessesLeft = 9;
+            guesses = [];
+        } else if (guessesLeft === 0){
+            losses++;
+            guessesLeft = 9;
+            guesses = [];
+        } else {
+            guesses.push(userGuess);
+            guesses.join(", ");
+            console.log(guesses);
+            guessesLeft--;
         }
-
-        winsText.textContent = "Wins: " + wins;
-        lossesText.textContent = "Losses: " + losses;
-        guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
-        guessesText.textContent = "Your Guesses so far: " + userGuess;
     }
+
+    winsText.textContent = wins;
+    lossesText.textContent = losses;
+    guessesLeftText.textContent = guessesLeft;
+    guessesText.textContent = guesses;
+    guessesText.textContent = userGuess;
+
 };
+
+// if (userGuess === computerGuess){
+        //     wins++; guessesLeft = 9; textGuess = "";
+        // }
+
+        // for (var i = 0; i <= guessesLeft; i++) {
+        //     if (userGuess !== computerGuess){
+        //         guesses.append(userGuess);
+        //         guessesLeft--;
+
+        //     } else if (guessesLeft = 0){
+        //         Losses++; guessesLeft = 9; textGuess = "";
+        //     }
+        // }
+
+        // if (userGuess !== computerGuess) {
+        //     guesses.push(userGuess);
+        //     guessesLeft--;
+        // } else if (guessesLeft = 0) {
+        //     losses++;
+        //     guessesLeft = 9;
+        //     guesses = [];
+        // } else if (userGuess === computerGuess) {
+        //     wins++ ,
+        //         guessesLeft = 9,
+        //         guesses = [];
+        // }
+
+// if (userGuess !== computerGuess){
+    //     guesses.push(userGuess),
+    //     guessesLeft --;
+    //     console.log(guesses);
+    //     if (userGuess === computerGuess){
+    //         wins++, guessesLeft = 9, guesses = [];
+    //     } else if (guessesLeft = 0) {
+    //         losses--, guessesLeft = 9, guesses = [];
+    //     }
+
 
